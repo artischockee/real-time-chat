004103b6a401a39718840598495adafb8c9d343a
artyeug <artyeug@gmail.com>
2018-09-02 14:11:31 +0700

    Rename the file

--- src/js/app_comp.js
+++ src/js/app_comp.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import HeaderContainer from './header/containers/header';
+import HeaderContainer from './header/header';
 import MainContainer from './main/containers/main';
-import FooterContainer from './footer/containers/footer';
+import FooterContainer from './footer/footer';
 
 const AppComponent = (props) => {
   return (
     <React.Fragment>
       {props.langChooserElement}
 
-      <HeaderContainer lang={props.language} />
+      <HeaderContainer
+        lang={props.language}
+        mayBeDisabled={props.sectionMayBeDisabled}
+      />
 
       <MainContainer
         confirmLogIn={props.confirmLogIn}
@@ -17,7 +20,7 @@ const AppComponent = (props) => {
       />
 
       <FooterContainer
-        mayBeDisabled={props.footerMayBeDisabled}
+        mayBeDisabled={props.sectionMayBeDisabled}
         lang={props.language}
       />
     </React.Fragment>
