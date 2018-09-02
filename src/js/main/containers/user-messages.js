4dc892e33e719c471e9e59c7dc925702972f3e0c
artyeug <artyeug@gmail.com>
2018-09-02 14:14:52 +0700

    Add shouldComponentUpdate() method

--- src/js/main/containers/user-messages.js
+++ src/js/main/containers/user-messages.js
@@ -2,6 +2,10 @@ import React from 'react';
 import autobind from 'autobind-decorator';
 import UserMessages from '../components/user-messages';
 
+function arraysEqual(first, second) {
+  return first.every((item, index) => item === second[index]);
+}
+
 @autobind
 export default class UserMessagesContainer extends React.Component {
   constructor(props) {
@@ -21,8 +25,11 @@ export default class UserMessagesContainer extends React.Component {
       return null;
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('UserMessages updated.');
+  shouldComponentUpdate(nextProps, nextState) {
+    if (arraysEqual(nextProps.messages, this.props.messages))
+      return false;
+
+    return true;
   }
 
   render() {
