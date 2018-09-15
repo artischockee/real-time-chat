b645ef9641dc403a32c9abd09de1fd71f826f7bc
artyeug <artyeug@gmail.com>
2018-09-15 23:54:58 +0700

    Replace most of the code from old sources here

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import autobind from 'autobind-decorator';
 import { hot } from 'react-hot-loader';
-import AppComponent from './app_comp';
-import LangChooserContainer from './language-chooser/lang-chooser';
+import EntryContainer from './entry/containers/entry';
+import ChatContainer from './chat/containers/chat';
+import { LANGUAGE } from './auxiliary/language';
+import { WS_STATES, CALLBACK_STATE } from './auxiliary/states';
+
+const HOSTNAME = window.location.hostname;
+const PORT = window.location.port;
+const COLON = PORT === '' ? '' : ':';
+const PROTOCOL = window.location.protocol;
+const WS_PROTOCOL = PROTOCOL === 'http:' ? 'ws:' : 'wss:';
+const SERVER_URL = `${WS_PROTOCOL}//${HOSTNAME}${COLON}${PORT}`;
+
+const MSG_TYPES = {
+  MESSAGE: 'MESSAGE',
+  ID: 'ID',
+  USERDATA: 'USERDATA',
+  USERLIST: 'USERLIST',
+  REJECT_USERDATA: 'REJECT_USERDATA'
+};
+
+const FRAGMENTS = {
+  ENTRY: 'ENTRY',
+  CHAT: 'CHAT'
+};
 
 @autobind
 class App extends React.Component {
@@ -10,38 +32,115 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      displayLangChooser: true,
-      sectionMayBeDisabled: false,
-      language: 'en'
+      chatMessages: [],
+      connection: null,
+      clientID: 0,
+      currentFragment: FRAGMENTS.ENTRY,
+      language: LANGUAGE.EN.SHORT,
+      message: '',
+      usersOnline: [],
+      userData: {
+        login: '',
+        sign: ''
+      }
     };
   }
 
-  confirmLogIn() {
+  handleLangChange(language) {
+    switch (language) {
+      case undefined:
+      case null:
+        return;
+    }
+
+    this.setState({ language });
+  }
+
+  applyLoginBoxData() {
+    let message = {
+      type: MSG_TYPES.USERDATA,
+      date: Date.now(),
+      id: this.state.clientID,
+      login: this.state.userData.login,
+      sign: this.state.userData.sign
+    };
+
+    this.state.connection.send(JSON.stringify(message));
+  }
+
+  connect() {
+    if (this.state.connection !== null) {
+      console.error('You have already established the connection.');
+      return;
+    }
+
+    let connection = new WebSocket(SERVER_URL);
+
+    connection.onopen = event => {
+      console.log('WS connection opened.');
+    };
+
+    connection.onmessage = event => {
+      let message = JSON.parse(event.data);
+
+      switch (message.type) {
+        case MSG_TYPES.ID:
+          this.setState({ clientID: message.id });
+          this.applyLoginBoxData();
+          break;
+        case MSG_TYPES.USERLIST:
+          this.setState({
+            usersOnline: message.users
+          });
+          break;
+        case MSG_TYPES.MESSAGE:
+          let chatMessages = [...this.state.chatMessages, message];
+          this.setState({ chatMessages });
+          break;
+        case MSG_TYPES.REJECT_USERDATA:
+          alert(`Your login has been changed to ${message.login} because previous name is already taken.`);
+          break;
+      }
+    };
+
+    connection.onclose = event => {
+      console.log('WebSocket connection has been closed.');
+    }
+
+    connection.onerror = event => {
+      console.error('WebSocket error observed:', event);
+    }
+
     this.setState({
-      sectionMayBeDisabled: true
+      connection,
+      currentFragment: FRAGMENTS.CHAT
     });
   }
 
-  handleLangChooserSubmit(language) {
-    if (language === '')
+  sendMessage() {
+    if (this.state.message === '')
       return;
 
-    this.setState({ language });
+    let message = {
+      type: MSG_TYPES.MESSAGE,
+      text: this.state.message,
+      id: this.state.clientID,
+      date: Date.now()
+    };
 
-    setTimeout(() => {
-      this.setState({ displayLangChooser: false });
-    }, 1000);
+    this.state.connection.send(JSON.stringify(message));
+
+    this.setState({ message: '' });
   }
 
-  showLangChooser() {
-    if (this.state.displayLangChooser)
-      return (
-        <LangChooserContainer
-          handleSubmit={this.handleLangChooserSubmit}
-        />
-      );
+  handleLogInInputChange(event) {
+    let keyName = event.target.name;
 
-    return null;
+    let userData = this.state.userData;
+
+    userData[keyName] = event.target.value;
+
+    this.setState({ userData });
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -49,14 +148,24 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <AppComponent
-        confirmLogIn={this.confirmLogIn}
-        sectionMayBeDisabled={this.state.sectionMayBeDisabled}
-        langChooserElement={this.showLangChooser()}
-        language={this.state.language}
-      />
-    );
+    switch (this.state.currentFragment) {
+      case FRAGMENTS.ENTRY:
+        return (
+          <EntryContainer
+            handleConnect={this.connect}
+            handleLangChange={this.handleLangChange}
+            handleLogInInputChange={this.handleLogInInputChange}
+            lang={this.state.language}
+            userData={this.state.userData}
+          />
+        );
+      case FRAGMENTS.CHAT:
+        return (
+          <ChatContainer />
+        );
+      default:
+        throw new Error('Could not find an appropriate fragment.');
+    }
   }
 }
 
