f77ade574feb4d7245285492600bbe762f98f149
artyeug <artyeug@gmail.com>
2018-09-15 23:53:59 +0700

    Remove all unnecessary code (that was partially moved into new app.js)

--- src/js/main/containers/main.js
+++ src/js/main/containers/main.js
@@ -1,144 +1,9 @@
 import React from 'react';
 import autobind from 'autobind-decorator';
 import Main from '../components/main';
-import { WS_STATES, CALLBACK_STATE } from '../../auxiliary/states';
-
-const HOSTNAME = window.location.hostname;
-const PORT = window.location.port;
-const COLON = PORT === '' ? '' : ':';
-const PROTOCOL = window.location.protocol;
-const WS_PROTOCOL = PROTOCOL === 'http:' ? 'ws:' : 'wss:';
-const SERVER_URL = `${WS_PROTOCOL}//${HOSTNAME}${COLON}${PORT}`;
-
-const MSG_TYPES = {
-  MESSAGE: 'MESSAGE',
-  ID: 'ID',
-  USERDATA: 'USERDATA',
-  USERLIST: 'USERLIST',
-  REJECT_USERDATA: 'REJECT_USERDATA'
-};
-
-function getRandomUserImageSrc() {
-  let imageNumber = Math.floor(Math.random() * 20) + 1;
-
-  return `images/avatars/${imageNumber}.jpg`;
-}
 
 @autobind
 export default class MainContainer extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      connection: null,
-      clientID: 0,
-      message: '',
-      userData: {
-        login: '',
-        sign: '',
-        image: ''
-      },
-      usersOnline: [],
-      chatMessages: [],
-      controlsAreFrozen: true,
-      onlineSectionHidden: true,
-      displayLoginBox: true
-    };
-  }
-
-  getConnectionState() {
-    if (this.state.connection === null
-    || this.state.connection === undefined)
-      return null;
-
-    return this.state.connection.readyState;
-  }
-
-  applyLoginBoxData() {
-    let userData = this.state.userData;
-    if (userData.image === '')
-      userData.image = getRandomUserImageSrc();
-
-    this.setState({ userData });
-
-    let message = {
-      type: MSG_TYPES.USERDATA,
-      date: Date.now(),
-      id: this.state.clientID,
-      login: this.state.userData.login,
-      sign: this.state.userData.sign,
-      image: this.state.userData.image
-    };
-
-    this.state.connection.send(JSON.stringify(message));
-  }
-
-  connect() {
-    if (this.state.connection !== null) {
-      console.error('You have already established the connection.');
-      return;
-    }
-
-    let connection = new WebSocket(SERVER_URL);
-
-    connection.onopen = event => {
-      this.setState({
-        controlsAreFrozen: false
-      });
-    };
-
-    connection.onmessage = event => {
-      let message = JSON.parse(event.data);
-
-      switch (message.type) {
-        case MSG_TYPES.ID:
-          this.setState({ clientID: message.id });
-          this.applyLoginBoxData();
-          break;
-        case MSG_TYPES.USERLIST:
-          this.setState({
-            usersOnline: message.users
-          });
-          break;
-        case MSG_TYPES.MESSAGE:
-          let chatMessages = [...this.state.chatMessages, message];
-          this.setState({ chatMessages });
-          break;
-        case MSG_TYPES.REJECT_USERDATA:
-          alert(`Your login has been changed to ${message.login} because previous name is already taken.`);
-          break;
-      }
-    };
-
-    connection.onclose = event => {
-      console.log('WebSocket connection has been closed.');
-    }
-
-    connection.onerror = event => {
-      console.error('WebSocket error observed:', event);
-    }
-
-    this.setState({ connection });
-
-    this.props.confirmLogIn();
-  }
-
-  sendMessage() {
-    if (this.state.message === '')
-      return;
-
-    let message = {
-      type: MSG_TYPES.MESSAGE,
-      text: this.state.message,
-      id: this.state.clientID,
-      date: Date.now()
-    };
-
-    this.state.connection.send(JSON.stringify(message));
-
-    this.setState({ message: '' });
-  }
-
   chatHandleMessageBoxChange(event) {
     this.setState({
       message: event.target.value
@@ -148,16 +13,6 @@ export default class MainContainer extends React.Component {
   chatHandleMessageBoxEnterKeyPress(event) {
     if (!this.state.controlsAreFrozen && event.key === 'Enter')
       this.sendMessage();
-  }
-
-  chatHandleLoginBoxChange(event) {
-    let keyName = event.target.name;
-
-    let userData = this.state.userData;
-
-    userData[keyName] = event.target.value;
-
-    this.setState({ userData });
   }
 
   getLoginBoxCallback(state) {
@@ -198,14 +53,11 @@ export default class MainContainer extends React.Component {
       <Main
         chatClientID={this.state.clientID}
         chatControlsAreFrozen={this.state.controlsAreFrozen}
-        chatHandleConnect={this.connect}
-        chatHandleLoginBoxChange={this.chatHandleLoginBoxChange}
         chatHandleMessageSending={this.sendMessage}
         chatHandleMessageBoxChange={this.chatHandleMessageBoxChange}
         chatHandleMessageBoxEnterKeyPress={this.chatHandleMessageBoxEnterKeyPress}
         chatMessages={this.state.chatMessages}
         chatMessageValue={this.state.message}
-        connectionState={this.getConnectionState()}
         displayLoginBox={this.state.displayLoginBox}
         lang={this.props.lang}
         loginBoxCallback={this.getLoginBoxCallback}
