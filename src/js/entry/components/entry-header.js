f85d59418a289ea43d509ef946aa9b4dcaf305b3
artyeug <artyeug@gmail.com>
2018-09-19 13:45:25 +0700

    Replace <img /> with universal SVGLogo class

--- src/js/entry/components/entry-header.js
+++ src/js/entry/components/entry-header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SVGLogo from '../../svg-components/logo';
 
 const EntryHeader = (props) => {
   return (
     <div className="entry-header">
-      <img className="entry-header__logo" src="images/logo.svg" alt="Logo" />
+      <SVGLogo className="entry-header__logo" />
       <h1 className="entry-header__title">Real-time chat</h1>
       <ul className="entry-header__list">
         {props.listItems}
