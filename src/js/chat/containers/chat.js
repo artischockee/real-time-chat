508c162c19996f2b923267fa74287e14f7d06b5b
artyeug <artyeug@gmail.com>
2018-09-20 01:17:24 +0700

    Remove handleControlClick() method

--- src/js/chat/containers/chat.js
+++ src/js/chat/containers/chat.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import autobind from 'autobind-decorator';
 import Chat from '../components/chat';
-import { BTN_NAMES } from '../../auxiliary/chat-button-names';
 
 const NOTIFICATION_SOUND = new Audio('appointed.mp3');
 
@@ -15,19 +14,6 @@ export default class ChatContainer extends React.Component {
     }
   }
 
-  handleControlClick(event) {
-    switch (event.target.name) {
-      case BTN_NAMES.NOTIFICATIONS:
-
-        break;
-      case BTN_NAMES.DEL_MESSAGES:
-
-        break;
-      default:
-
-    }
-  }
-
   shouldNotificationSoundPlay(state) {
     this.setState({
       notificationSoundShouldPlay: state
@@ -36,7 +22,7 @@ export default class ChatContainer extends React.Component {
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     console.log('Chat updated.');
-    
+
     if (
       prevProps.messages !== this.props.messages
       && this.props.messages[this.props.messages.length - 1].id !== this.props.clientID
@@ -48,9 +34,9 @@ export default class ChatContainer extends React.Component {
   render() {
     return (
       <Chat
-        handleControlClick={this.handleControlClick}
         handleMsgBoxChange={this.props.handleMsgBoxChange}
         handleMsgBoxKeyUp={this.props.handleMsgBoxKeyUp}
+        handleMsgDelete={this.props.handleMsgDelete}
         lang={this.props.lang}
         messages={this.props.messages}
         messageValue={this.props.messageValue}
