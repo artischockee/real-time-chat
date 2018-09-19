61f43c3a95c036442c7032abd566761814b5d533
artyeug <artyeug@gmail.com>
2018-09-19 18:23:59 +0700

    Implement a notification sound

--- src/js/chat/containers/chat.js
+++ src/js/chat/containers/chat.js
@@ -3,10 +3,16 @@ import autobind from 'autobind-decorator';
 import Chat from '../components/chat';
 import { BTN_NAMES } from '../../auxiliary/chat-button-names';
 
+const NOTIFICATION_SOUND = new Audio('appointed.mp3');
+
 @autobind
 export default class ChatContainer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      notificationSoundShouldPlay: true
+    }
   }
 
   handleControlClick(event) {
@@ -22,16 +28,33 @@ export default class ChatContainer extends React.Component {
     }
   }
 
+  shouldNotificationSoundPlay(state) {
+    this.setState({
+      notificationSoundShouldPlay: state
+    });
+  }
+
   componentDidUpdate(prevProps, prevState, snapshot) {
     console.log('Chat updated.');
+    
+    if (
+      prevProps.messages !== this.props.messages
+      && this.props.messages[this.props.messages.length - 1].id !== this.props.clientID
+      && this.state.notificationSoundShouldPlay
+    )
+      NOTIFICATION_SOUND.play();
   }
 
   render() {
     return (
       <Chat
         handleControlClick={this.handleControlClick}
+        handleMsgBoxChange={this.props.handleMsgBoxChange}
+        handleMsgBoxKeyUp={this.props.handleMsgBoxKeyUp}
         lang={this.props.lang}
         messages={this.props.messages}
+        messageValue={this.props.messageValue}
+        shouldNotificationSoundPlay={this.shouldNotificationSoundPlay}
         userList={this.props.userList}
       />
     );
