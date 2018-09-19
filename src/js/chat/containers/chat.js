30681984ccf8ea66deefbc3ec12de43e668237ed
artyeug <artyeug@gmail.com>
2018-09-19 13:47:42 +0700

    Add several props to Chat; add handleControlClick() method

--- src/js/chat/containers/chat.js
+++ src/js/chat/containers/chat.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import autobind from 'autobind-decorator';
 import Chat from '../components/chat';
+import { BTN_NAMES } from '../../auxiliary/chat-button-names';
 
 @autobind
 export default class ChatContainer extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  handleControlClick(event) {
+    switch (event.target.name) {
+      case BTN_NAMES.NOTIFICATIONS:
+
+        break;
+      case BTN_NAMES.DEL_MESSAGES:
+
+        break;
+      default:
+
+    }
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -14,7 +28,12 @@ export default class ChatContainer extends React.Component {
 
   render() {
     return (
-      <Chat />
+      <Chat
+        handleControlClick={this.handleControlClick}
+        lang={this.props.lang}
+        messages={this.props.messages}
+        userList={this.props.userList}
+      />
     );
   }
 }
