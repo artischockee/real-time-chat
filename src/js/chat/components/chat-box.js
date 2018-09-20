1930e19b572d206fb33304ed5b4b08ada1274943
artyeug <artyeug@gmail.com>
2018-09-20 15:09:28 +0700

    Add ref and onScroll event to the wrapper

--- src/js/chat/components/chat-box.js
+++ src/js/chat/components/chat-box.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 
 const ChatBox = props => {
   return (
-    <div className="main-panel__chat-container">
+    <div
+      className="main-panel__chat-container"
+      onScroll={props.handleScroll}
+      ref={props.windowRef}>
 
       {props.emptyChatPlaceholder}
       {props.messages}
@@ -14,6 +17,7 @@ const ChatBox = props => {
 
 ChatBox.propTypes = {
   emptyChatPlaceholder: PropTypes.object,
+  handleScroll: PropTypes.func.isRequired,
   messages: PropTypes.arrayOf(PropTypes.object)
 };
 
