4b1f0e45e44e26c7ba0f3c340c3d95821d12fcb1
artyeug <artyeug@gmail.com>
2018-09-20 13:21:51 +0700

    Implement search

--- src/js/chat/containers/side-panel.js
+++ src/js/chat/containers/side-panel.js
@@ -6,6 +6,29 @@ import SidePanel from '../components/side-panel';
 export default class SidePanelContainer extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchValue: ''
+    }
+  }
+
+  getFilteredUserList() {
+    if (this.state.searchValue === '')
+      return this.props.userList;
+
+    return this.props.userList.filter(user => {
+      let login = user.login.toLowerCase();
+      let sign = user.sign.toLowerCase();
+      let searchValue = this.state.searchValue.toLowerCase();
+
+      return login.includes(searchValue) || sign.includes(searchValue);
+    });
+  }
+
+  handleSearchChange(event) {
+    this.setState({
+      searchValue: event.target.value
+    });
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -15,7 +38,9 @@ export default class SidePanelContainer extends React.Component {
   render() {
     return (
       <SidePanel
-        userList={this.props.userList}
+        handleSearchChange={this.handleSearchChange}
+        searchValue={this.state.searchValue}
+        userList={this.getFilteredUserList()}
       />
     );
   }
