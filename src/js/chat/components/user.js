0decc7c0aa3e8a7619e5994e03979c07db1d9410
artyeug <artyeug@gmail.com>
2018-10-04 02:16:50 +0700

    Improve alt on the user image

--- src/js/chat/components/user.js
+++ src/js/chat/components/user.js
@@ -5,7 +5,7 @@ const User = props => {
   return (
     <li className="user-list__user">
       <div className="user__image-wrapper">
-        <img className="user__image" src={props.imgSrc} alt="avatar" />
+        <img className="user__image" src={props.imgSrc} alt={`${props.login}\'s avatar`} />
       </div>
       <div className="user__info-wrapper">
         <p className="user__name">{props.login}</p>
