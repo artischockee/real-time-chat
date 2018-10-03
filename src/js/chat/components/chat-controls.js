269ee0858927fba28f077eb0f64690d540d45e81
artyeug <artyeug@gmail.com>
2018-10-03 23:58:40 +0700

    Add new menu button

--- src/js/chat/components/chat-controls.js
+++ src/js/chat/components/chat-controls.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SVGTrashCan from '../../svg-components/trash-can';
+import SVGHamburger from '../../svg-components/hamburger';
 import { APP_NAME } from '../../auxiliary/app-name';
 import { CHAT_BUTTONS } from '../../auxiliary/button-names';
 
 const ChatControls = props => {
   return (
     <div className="main-panel__controls-container">
+
+      <button name={CHAT_BUTTONS.MENU} className="chat__control-button chat__control-button_menu" onClick={props.handleClick}>
+        <SVGHamburger className="control-button__svg" />
+      </button>
 
       <p className="main-panel__inscription">{APP_NAME}</p>
 
