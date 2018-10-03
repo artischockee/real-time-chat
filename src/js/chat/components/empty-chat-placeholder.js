a36fceadba0356dc857df44be21f78775a5f83db
artyeug <artyeug@gmail.com>
2018-10-03 23:58:29 +0700

    Make SVGLogo one-lined

--- src/js/chat/components/empty-chat-placeholder.js
+++ src/js/chat/components/empty-chat-placeholder.js
@@ -6,13 +6,12 @@ const EmptyChatPlaceholder = props => {
   return (
     <div className="empty-chat-placeholder">
 
-      <SVGLogo
-        className="empty-chat-placeholder__svg"
-      />
+      <SVGLogo className="empty-chat-placeholder__svg" />
 
       <p className="empty-chat-placeholder__text">
         {props.inscription}
       </p>
+      
     </div>
   );
 };
